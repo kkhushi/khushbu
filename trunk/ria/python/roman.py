ed8b884f41708de714e228b388f00ae731be34a6
riadas <riadas@7fddab9f-8852-0410-a958-bbde0b0c641b>
2008-07-21 17:37:06 +0000

    decimal to roman program added

--- trunk/ria/python/roman.py
+++ trunk/ria/python/roman.py
@@ -4,62 +4,66 @@ a = []
 s = []
 count = 0
 n = int(raw_input("Enter the decimal number:"))
-while n > 0:
-    x = n%10
-    a.append(x)
-    count = count + 1
-    n = n/10
+if n < 3999:
+    while n > 0:
+        x = n%10
+        a.append(x)
+        count = count + 1
+        n = n/10
     
-if count>0:
-    cha2 = 'I'
+    if count>0:
+        cha2 = 'I'
     
-    if a[0] == 4:
-        char1 = 'IV'
-    elif a[0]>=5 and a[0]<=8:
-        cha1 = 'V'
-        char1 = cha1 + cha2*(a[0]-5)
-    elif a[0] == 9:
-        char1 = 'IX'
-    else:
-        char1 = cha2*a[0]
-    s.append(char1)
-
-if count>1:
-    cha2 = 'X'
-    
-    if a[1] == 4:
-        char1 = 'XL'
-    elif a[1]>=5 and a[1]<=8:
-        cha1 = 'L'
-        char1 = cha1 + cha2*(a[1]-5)
-    elif a[1] == 9:
-        char1 = 'XC' 
-    else:
-        char1 = cha2*a[1]
+        if a[0] == 4:
+            char1 = 'IV'
+        elif a[0]>=5 and a[0]<=8:
+            cha1 = 'V'
+            char1 = cha1 + cha2*(a[0]-5)
+        elif a[0] == 9:
+            char1 = 'IX'
+        else:
+            char1 = cha2*a[0]
         s.append(char1)
 
-if count>2:
-    cha2 = 'C'
+    if count>1:
+        cha2 = 'X'
     
-    if a[2] == 4:
-        char1 = 'CD'
-    elif a[2]>=5 and a[2]<=8:
-        cha1 = 'D'
-        char1 = cha1 + cha2*(a[2]-5)
-    elif a[2] == 9:
-        char1 = 'CM'
-    else:
-        char1 = cha2*a[2]
-    s.append(char1)
+        if a[1] == 4:
+            char1 = 'XL'
+        elif a[1]>=5 and a[1]<=8:
+            cha1 = 'L'
+            char1 = cha1 + cha2*(a[1]-5)
+        elif a[1] == 9:
+            char1 = 'XC' 
+        else:
+            char1 = cha2*a[1]
+        s.append(char1)
 
-if count>3:
-    cha2 = 'M'
+    if count>2:
+        cha2 = 'C'
     
-    char1 = cha2*a[3]
-    s.append(char1)
+        if a[2] == 4:
+            char1 = 'CD'
+        elif a[2]>=5 and a[2]<=8:
+            cha1 = 'D'
+            char1 = cha1 + cha2*(a[2]-5)
+        elif a[2] == 9:
+            char1 = 'CM'
+        else:
+            char1 = cha2*a[2]
+        s.append(char1)
+
+    if count>3:
+        cha2 = 'M'
+    
+        char1 = cha2*a[3]
+        s.append(char1)
 
 
-s.reverse()
-s = "".join(s)
-print s
+    s.reverse()
+    s = "".join(s)
+    print s
+
+else:
+    print("invalid number")
 
