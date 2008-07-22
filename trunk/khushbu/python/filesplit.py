502b8c25e5fd6985ac006f305978be6a25b715f5
khushbu <khushbu@7fddab9f-8852-0410-a958-bbde0b0c641b>
2008-07-22 19:34:29 +0000

    Updated file splitter code

--- trunk/khushbu/python/filesplit.py
+++ trunk/khushbu/python/filesplit.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
-import os
+import os, math
 
 name = raw_input("Enter the filename or path:\t")
-f = open(name, 'r')
-data = f.read()
-fsize = data.__len__()
-csize = size//5
+if os.path.exists(name):
+  f = open(name, 'r')
+  data = f.read()
+  fsize = len(data)
+  csize = fsize/5 +1
+  f.seek(0)
+  for i in range(0,5):
+    s = name +str(i)
+    new  = open(s,'w')
+    new.write(f.read(csize))
+    print s 
+  os.remove(name)
+else:
+  print "file doesn't exist"
 
 
