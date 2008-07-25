b182264e01858c33d0a001806d7d2adaea332894
amrita <amrita@7fddab9f-8852-0410-a958-bbde0b0c641b>
2008-07-25 13:26:49 +0000

    added hello3.c

---
+++ trunk/amrita/hello3.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <math.h>
+#define no 9
+
+int main ()
+{
+    int n = 9;
+
+    printf ("Hello, world\n");
+    printf ("square root(%d): %f\n", n, sqrt (9));
+  
+    return 0;
+}
+                                                                               
+              
