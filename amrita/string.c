1f2ca573f67f61004de86aa136f8d774695045b6
amrita <amrita@7fddab9f-8852-0410-a958-bbde0b0c641b>
2008-08-02 13:36:25 +0000

    string.c added

---
+++ amrita/string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+  { int i;
+    char a[20];
+    printf("enter the string:");
+    scanf("%s",a);
+    printf("the entered string is: %s",a);
+    for(i=0;a[i]!='\0';i++)
+     { }
+    printf("the length is %d",i);
+    exit(0);
+}
+ 
