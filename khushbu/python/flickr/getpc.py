e5669df1ef9fb5ad0ffa48b95c5afa1e2153666e
khushbu <khushbu@7fddab9f-8852-0410-a958-bbde0b0c641b>
2008-08-06 15:09:45 +0000

    little improvement in getpc

--- khushbu/python/flickr/getpc.py
+++ khushbu/python/flickr/getpc.py
@@ -7,30 +7,28 @@ u = raw_input("Enter user_id:\t")
 s = raw_input("Enter set to download:\t")
 
 d = s
+
 try:
     os.mkdir(d)
 except:
     print "Directory already exists"
 
+#try:
 user = flickr.User(u)
 set = user.getPhotosets()
-
 for photoset in set:
   if photoset.title == s:
     set = photoset
-
 photos = set.getPhotos()
 
 for photo in photos:
-  url = photo.getURL(size ='Large')
-  link = urllib2.urlopen(url)
-  data = link.read()
-  x = data.find('<p><img src="')
-  a = data[x+13 :x +76 ]
+  url = photo.getURL(size ='Large', urlType = 'source')
   opener1 = urllib2.build_opener()
-  page1 = opener1.open(a)
+  page1 = opener1.open(url)
   my_picture = page1.read()
   filename = os.path.join(d,photo.title + ".jpg")
   fout = open(filename, "wb")
   fout.write(my_picture)
   fout.close()
+#except:
+# print"Error connecting... Try again"
